951fd26b55bd6f9951469688bf85e33410674efe
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2024-08-20 17:22:34 +0200

    def

---
+++ ServerInfo.ps1
@@ -0,0 +1,24 @@
+﻿$ServerInfo = @(
+                    [PSCustomObject]@{
+                                        ServerName = "MGMT01"
+                                        MaintenanceModeActive = "False"
+                                        Monitoring_WindowsServices = @(
+                                                                        [PSCustomObject]@{
+                                                                                            Type = "WindowsServices"
+                                                                                            ServiceDisplayNameContains = @("Kiwi","WindowsUpdate")
+                                                                                         }
+                                                                      )
+                                     }
+                    [PSCustomObject]@{
+                                        ServerName = "MGMT02"
+                                        MaintenanceModeActive = "True"
+                                        Monitoring_WindowsServices = @(
+                                                                        [PSCustomObject]@{
+                                                                                            Type = "WindowsServices"
+                                                                                            ServiceDisplayNameContains = @("WindowsUpdate")
+                                                                                         }
+                                                                      )
+                                     }
+               )
+
+$JSON = $ServerInfo | ConvertTo-Json -Depth 20 | out-file "C:\Users\mok\OneDrive - 2linkIT\Documents\GitHub\PowershellKnownMitigationsLib\ServerMonitoring.json" -Encoding utf8 -Force
